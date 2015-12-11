2f2f3d0f6c15966bec58348c393d1ee4d1778775
Ronan Barzic <rbarzic@gmail.com>
2015-12-11 11:06:42 +0100

    Fix aligment problem with GCC - Remove some warnings

--- projects/hack_day_boat_plane/main.c
+++ projects/hack_day_boat_plane/main.c
@@ -1,4 +1,3 @@
-
 #include <nrf.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -16,7 +15,9 @@
 #define SERVO_MIN ((1UL << 15) | 1100)
 #define SERVO_MID ((1UL << 15) | 1500)
 
-uint16_t m_buf[] = {SERVO_MID, SERVO_MID, SERVO_MID};
+uint32_t m_buf32[2];
+uint16_t* m_buf;
+
 uint16_t m_pitch = 500;
 uint16_t m_yaw = 500;
 uint16_t m_roll = 500;
@@ -35,7 +36,7 @@ bool pitch(uint16_t value)
   if (value < 1000)
   {
     m_pitch = value;
-    
+
     return true;
   }
   return false;
@@ -46,7 +47,7 @@ bool roll(uint16_t value)
   if (value < 1000)
   {
     m_roll = value;
-    
+
     return true;
   }
   return false;
@@ -66,14 +67,14 @@ void update_servos()
 {
   uint16_t left_flap, right_flap;
   m_buf[RUDDER] = (1UL << 15) | (1000UL + m_yaw);
-  
-  
-  left_flap = (1UL << 15) | ((m_roll + m_pitch) / 2) + 1000;
-  right_flap = (1UL << 15) | ((m_roll + (1000 - m_pitch)) / 2) + 1000;
-  //m_buf[LEFT_FLAPS] = m_buf[RIGHT_FLAPS] = (1UL << 15) | (1000 + m_roll);  
+
+
+  left_flap = (1UL << 15) | (((m_roll + m_pitch) / 2) + 1000);
+  right_flap = (1UL << 15) | (((m_roll + (1000 - m_pitch)) / 2) + 1000);
+  //m_buf[LEFT_FLAPS] = m_buf[RIGHT_FLAPS] = (1UL << 15) | (1000 + m_roll);
   //m_buf[LEFT_FLAPS] = (1UL << 15) | (1000 + m_roll);
   //m_buf[RIGHT_FLAPS] = (1UL << 15) | (1000 + (1000 - m_roll));
-  
+
   m_buf[LEFT_FLAPS] = left_flap;
   m_buf[RIGHT_FLAPS] = right_flap;
 
@@ -82,51 +83,55 @@ void update_servos()
 int main(void)
 {
   uint32_t tmo;
-  
+
   NRF_CLOCK->TASKS_HFCLKSTART = 1;
   while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
   NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
-  
+
+  m_buf = (uint16_t*)&m_buf32[0];
+  for(int i=0;i<4;i++)
+      m_buf[i] = SERVO_MID;
+
   NRF_GPIO->DIRSET = (1 << PWM_PIN_0) | (1 << PWM_PIN_1) | (1 << PWM_PIN_2);
   NRF_GPIO->OUTCLR = (1 << PWM_PIN_0) | (1 << PWM_PIN_1) | (1 << PWM_PIN_2);
-  
+
   NRF_GPIO->PIN_CNF[BUTTON1_PIN] = (GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos) |
-                                   (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos) | 
+                                   (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos) |
                                    (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos);
   NRF_GPIO->PIN_CNF[BUTTON2_PIN] = (GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos) |
-                                   (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos) | 
+                                   (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos) |
                                    (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos);
   NRF_GPIO->PIN_CNF[BUTTON3_PIN] = (GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos) |
-                                   (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos) | 
+                                   (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos) |
                                    (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos);
   NRF_GPIO->PIN_CNF[BUTTON4_PIN] = (GPIO_PIN_CNF_DIR_Input << GPIO_PIN_CNF_DIR_Pos) |
-                                   (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos) | 
+                                   (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos) |
                                    (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos);
-    
+
   NRF_PWM0->PRESCALER   = PWM_PRESCALER_PRESCALER_DIV_16; // 1 us
   NRF_PWM0->PSEL.OUT[0] = PWM_PIN_0;
   NRF_PWM0->PSEL.OUT[1] = PWM_PIN_1;
   NRF_PWM0->PSEL.OUT[2] = PWM_PIN_2;
   NRF_PWM0->MODE        = (PWM_MODE_UPDOWN_Up << PWM_MODE_UPDOWN_Pos);
-  NRF_PWM0->DECODER     = (PWM_DECODER_LOAD_Individual   << PWM_DECODER_LOAD_Pos) | 
+  NRF_PWM0->DECODER     = (PWM_DECODER_LOAD_Individual   << PWM_DECODER_LOAD_Pos) |
                           (PWM_DECODER_MODE_RefreshCount << PWM_DECODER_MODE_Pos);
   NRF_PWM0->LOOP      = (PWM_LOOP_CNT_Disabled << PWM_LOOP_CNT_Pos);
-  
+
   NRF_PWM0->COUNTERTOP = 20000; // 20ms period
-  
-  
+
+
   NRF_PWM0->SEQ[0].CNT = 4; //((sizeof(buf) / sizeof(uint16_t)) << PWM_SEQ_CNT_CNT_Pos);
   NRF_PWM0->SEQ[0].ENDDELAY = 0;
   NRF_PWM0->SEQ[0].PTR = (uint32_t)&m_buf[0];
   NRF_PWM0->SEQ[0].REFRESH = 0;
   NRF_PWM0->SHORTS = PWM_SHORTS_LOOPSDONE_SEQSTART0_Msk;
-  
+
   NRF_PWM0->ENABLE = 1;
-  
+
   NRF_PWM0->TASKS_SEQSTART[0] = 1;
   while (NRF_PWM0->EVENTS_SEQEND[0] == 0);
   NRF_PWM0->EVENTS_SEQEND[0] = 0;
-  
+
   while (1)
   {
     if ((NRF_GPIO->IN & (1 << BUTTON1_PIN)) == 0)
@@ -141,7 +146,7 @@ int main(void)
     {
       roll(500);
     }
-    
+
     if ((NRF_GPIO->IN & (1 << BUTTON3_PIN)) == 0)
     {
       pitch(100);
@@ -154,7 +159,7 @@ int main(void)
     {
       pitch(500);
     }
-    
+
     update_servos();
     NRF_PWM0->TASKS_SEQSTART[0] = 1;
     while (NRF_PWM0->EVENTS_SEQEND[0] == 0);

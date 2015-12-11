d6296b5a69048d6b42067dfce717fa14e8faa3be
Michael Dietz <Michael.Dietz@nordicsemi.no>
2015-12-11 10:10:01 +0100

    Initial Commit.

---
+++ projects/nRFModPlayeriOSApp/NordicSimpleBLE/BLEManager.swift
@@ -0,0 +1,140 @@
+//
+//  BLEManager.swift
+//  NordicSimpleBLE
+//
+//  Created by System Architecture on 27/10/15.
+//  Copyright © 2015 Nordic Semiconductor. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+protocol peripheralDelegate {
+    func didDiscoverPeripheral(peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber)
+    func didConnectPeripheral(peripheral: CBPeripheral)
+    func didDiscoverServices(peripheral : CBPeripheral, services : [CBService])
+    func didDiscoverCharacteristics(peripheral : CBPeripheral, characteristics : [CBCharacteristic])
+    func didUpdateValueForCharacteristic(peripheral: CBPeripheral, characteristic: CBCharacteristic)
+    func didDisconnectPeripheral(peripheral: CBPeripheral)
+}
+
+class BLEManager : NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    private var centralManager : CBCentralManager!
+    private var nRF5xPeripheral : CBPeripheral!
+    
+    var peripheralDelegateInst : peripheralDelegate?
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    /// Scans for peripherals that are advertising services.
+    func startScanning(UUIDs: [CBUUID]?) {
+        centralManager.scanForPeripheralsWithServices(UUIDs, options: nil)
+    }
+    
+    /// Asks the central manager to stop scanning for peripherals.
+    func stopScanning() {
+        centralManager.stopScan()
+    }
+    
+    /// Establishes a local connection to a peripheral.
+    func connectToPeripheral(peripheral: CBPeripheral) {
+        centralManager.stopScan()
+        nRF5xPeripheral = peripheral
+        nRF5xPeripheral.delegate = self
+        centralManager.connectPeripheral(nRF5xPeripheral, options: nil)
+    }
+    
+    /// Cancels an active or pending local connection to a peripheral.
+    func cancelConnectionToPeripheral(peripheral: CBPeripheral) {
+        centralManager.cancelPeripheralConnection(peripheral)
+    }
+    
+    /// Discovers the specified services of the peripheral.
+    func discoverServices(peripheral : CBPeripheral, services : [CBUUID]?) {
+        peripheral.discoverServices(services)
+    }
+    
+    /// Discovers the specified characteristics of a service.
+    func discoverCharacteristics(peripheral : CBPeripheral, service: CBService, characteristics : [CBUUID]?) {
+        peripheral.discoverCharacteristics(characteristics, forService: service)
+    }
+    
+    /// Discovers the descriptors of a characteristic.
+    func discoverDescriptors(characteristic: CBCharacteristic) {
+        nRF5xPeripheral.discoverDescriptorsForCharacteristic(characteristic)
+    }
+    
+    /// Sets notifications or indications for the value of a specified characteristic.
+    func setNotifyValue(characteristic: CBCharacteristic) {
+        nRF5xPeripheral.setNotifyValue(true, forCharacteristic: characteristic)
+    }
+    
+    /// Writes the value of a characteristic.
+    func writeCharacteristic(data: NSData, characteristic: CBCharacteristic) {
+        nRF5xPeripheral.writeValue(data, forCharacteristic: characteristic, type: CBCharacteristicWriteType.WithResponse)
+    }
+    
+    /// Retrieves the value of a specified characteristic.
+    func readCharacteristic(characteristic: CBCharacteristic) {
+        nRF5xPeripheral.readValueForCharacteristic(characteristic)
+    }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        switch (central.state) {
+        case .PoweredOff:
+            print("CoreBluetooth BLE hardware is powered off.")
+            
+        case .PoweredOn:
+            print("CoreBluetooth BLE hardware is powered on and ready.")
+            
+        case .Resetting:
+            print("CoreBluetooth BLE hardware is resetting.")
+            
+        case .Unauthorized:
+            print("CoreBluetooth BLE state is unauthorized.")
+            
+        case .Unknown:
+            print("CoreBluetooth BLE state is unknown.")
+            
+        case .Unsupported:
+            print("CoreBluetooth BLE hardware is unsupported on this platform.")
+        }
+    }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        peripheralDelegateInst?.didDiscoverPeripheral(peripheral, advertisementData: advertisementData, RSSI: RSSI)
+    }
+    
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        peripheralDelegateInst?.didConnectPeripheral(peripheral)
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        if let services = peripheral.services {
+            peripheralDelegateInst?.didDiscoverServices(peripheral, services: services)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+        if let characteristics = service.characteristics {
+            peripheralDelegateInst?.didDiscoverCharacteristics(peripheral, characteristics: characteristics)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverDescriptorsForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        // TODO: implement if needed or remove.
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        peripheralDelegateInst?.didUpdateValueForCharacteristic(peripheral, characteristic: characteristic)
+    }
+    
+    func centralManager(_: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        peripheralDelegateInst?.didDisconnectPeripheral(peripheral)
+    }
+
+}
---
+++ projects/nRFModPlayeriOSApp/NordicSimpleBLE/ViewController.swift
@@ -0,0 +1,178 @@
+//
+//  ViewController.swift
+//  NordicSimpleBLE
+//
+//  Created by System Architecture on 26/10/15.
+//  Copyright © 2015 Nordic Semiconductor. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ViewController: UIViewController, peripheralDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    private var bleManager = BLEManager()
+    
+    private let nRFModPlayerName = "nRF_Mod_Player"
+    
+    private let nRFModPlayerServiceUUID        = CBUUID.init(string: "00001523-1212-EFDE-1523-785FEABCD123")
+    private let nRFModPlayerCharacteristicUUID = CBUUID.init(string: "00001524-1212-EFDE-1523-785FEABCD123")
+    
+    private struct nordicBlueColor {
+        let red:   Float = 0.0   / 255.0
+        let green: Float = 156.0 / 255.0
+        let blue:  Float = 222.0 / 255.0
+        let alpha: Float = 1.0
+    }
+    
+    private let OffNS = NSData(bytes: [0] as [UInt8], length: sizeof(UInt8))
+    private let OnNS  = NSData(bytes: [1] as [UInt8], length: sizeof(UInt8))
+    
+    private var nRFModPlayerPeripheral          : CBPeripheral?
+    private var nRFModPlayerService             : CBService?
+    private var nRFModPlayerCharateristic       : CBCharacteristic?
+    private var nRFModPlayerCharacteristic      : CBCharacteristic?
+    
+    private var discoveredPeripherals = [(peripheral: CBPeripheral, RSSI: String)]()
+    
+    @IBOutlet weak var backgroundImage            : UIImageView!
+    @IBOutlet weak var blurOverlay                : UIImageView!
+    @IBOutlet weak var scanTableView              : UITableView!
+    @IBOutlet weak var navigationBar              : UINavigationItem!
+    @IBOutlet weak var scanDisconnectButtonOutlet : UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bleManager.peripheralDelegateInst = self
+        
+        scanTableView.rowHeight = UITableViewAutomaticDimension
+        scanTableView.estimatedRowHeight = 44.0 // Default table view cell is 44, but it doesn`t seem to matter what is actually put here.
+        
+        let nordicBlue = nordicBlueColor.init()
+        navigationController!.navigationBar.barTintColor = UIColor(colorLiteralRed: nordicBlue.red, green: nordicBlue.green, blue: nordicBlue.blue, alpha: nordicBlue.alpha)
+        navigationController!.navigationBar.tintColor = UIColor.whiteColor()
+        navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        
+        let blur = UIBlurEffect(style: .Dark)
+        let darkBlurView = UIVisualEffectView(effect: blur)
+        darkBlurView.frame = view.bounds
+        blurOverlay.addSubview(darkBlurView)
+        
+        hideScanWindow()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func scanDisconnectButtonAction(sender: UIButton) {
+        if let currentText = sender.currentTitle {
+            if currentText == "Scan" {
+                bleManager.startScanning([nRFModPlayerServiceUUID])
+                showScanWindow()
+            } else if currentText == "Disconnect" {
+                if let peripheral = nRFModPlayerPeripheral {
+                    bleManager.cancelConnectionToPeripheral(peripheral)
+                }
+            }
+        }
+    }
+    
+    /*@IBAction func toggleLED(sender: UITapGestureRecognizer) {
+        if let _ = nordicBlinkyPeripheral {
+            if ledState == false {
+                bleManager.writeCharacteristic(ledOnNS, characteristic: ledCharacteristic!)
+                lightBulb.image = UIImage(named: "bulb_on.png")
+            } else if ledState == true {
+                bleManager.writeCharacteristic(ledOffNS, characteristic: ledCharacteristic!)
+                lightBulb.image = UIImage(named: "bulb_off.png")
+            }
+            ledState = !ledState
+        }
+    }*/
+    
+    @IBAction func hideScanWindow(sender: UITapGestureRecognizer) {
+        // Should stop scanning.
+        hideScanWindow()
+    }
+    
+    func didDiscoverPeripheral(peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        discoveredPeripherals.append((peripheral, "\(RSSI)"))
+        scanTableView.reloadData()
+    }
+    
+    func didConnectPeripheral(peripheral: CBPeripheral) {
+        if peripheral.name == nRFModPlayerName {
+            nRFModPlayerPeripheral = peripheral
+            bleManager.discoverServices(peripheral, services: [nRFModPlayerServiceUUID])
+            scanDisconnectButtonOutlet.setTitle("Disconnect", forState: .Normal)
+            hideScanWindow()
+        }
+    }
+    
+    func didDiscoverServices(peripheral: CBPeripheral, services: [CBService]) {
+        for service in services {
+            if service.UUID == nRFModPlayerServiceUUID {
+                nRFModPlayerService = service
+                bleManager.discoverCharacteristics(peripheral, service: service, characteristics: [nRFModPlayerCharacteristicUUID])
+            }
+        }
+    }
+    
+    func didDiscoverCharacteristics(peripheral: CBPeripheral, characteristics: [CBCharacteristic]) {
+        for characteristic in characteristics {
+            if characteristic.UUID == nRFModPlayerCharacteristicUUID {
+                nRFModPlayerCharateristic = characteristic
+                bleManager.setNotifyValue(characteristic)
+            }
+        }
+    }
+    
+    func didUpdateValueForCharacteristic(peripheral: CBPeripheral, characteristic: CBCharacteristic) {
+        if let charValue = characteristic.value {
+            if charValue == OffNS {
+                backgroundImage.image = backgroundImage.image!.imageWithRenderingMode(.AlwaysOriginal)
+            } else if charValue == OnNS {
+                backgroundImage.image = backgroundImage.image!.imageWithRenderingMode(.AlwaysTemplate)
+            }
+        }
+    }
+    
+    func didDisconnectPeripheral(peripheral: CBPeripheral) {
+        nRFModPlayerPeripheral = nil
+        nRFModPlayerService = nil
+        nRFModPlayerCharateristic = nil
+        scanDisconnectButtonOutlet.setTitle("Scan", forState: .Normal)
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = scanTableView.dequeueReusableCellWithIdentifier("cell")!
+        let peripheral = discoveredPeripherals[indexPath.row].peripheral
+        let RSSI = discoveredPeripherals[indexPath.row].RSSI
+        cell.textLabel?.text = peripheral.name
+        cell.detailTextLabel?.text = "RSSI: \(RSSI)"
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return discoveredPeripherals.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        bleManager.connectToPeripheral(discoveredPeripherals[indexPath.row].peripheral)
+    }
+    
+    private func hideScanWindow() {
+        scanTableView.hidden = true
+        blurOverlay.hidden = true
+    }
+    
+    private func showScanWindow() {
+        discoveredPeripherals.removeAll()
+        scanTableView.reloadData()
+        blurOverlay.hidden = false
+        scanTableView.hidden = false
+    }
+    
+}
